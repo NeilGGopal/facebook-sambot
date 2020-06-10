6f57bb53e30a71bba519dd1614ab5aa471aa933f
Neil Gopal <gopaln@purdue.edu>
2020-06-10 16:47:44 -0700

    Created Account class with instance and example

--- sambot.py
+++ sambot.py
@@ -3,3 +3,10 @@ from fbchat import Client, Message
 from random import choice
 import json
 
+class Account(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        pass
+
+
+client = Account('email', get_pass())
+client.send(Message('we need sambar'), thread_id, ThreadType.USER)
